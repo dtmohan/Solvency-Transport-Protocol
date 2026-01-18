c02b7670cdca17c5a1bfb62f5d600a8c2b0eb579
Deepak <deepak.t.mohan@gmail.com>
2026-01-18 15:38:06 -0800

    Create __init__.py
    reference implementation scaffold

---
+++ stp/__init__.py
@@ -0,0 +1,8 @@
+# stp/__init__.py
+"""
+STP Governor package (reference implementation scaffold).
+
+This package is intentionally minimal and model-agnostic.
+See: stp/governor.py
+"""
+__all__ = ["governor"]
